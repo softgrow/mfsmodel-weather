568d4fab992d0ee04f39f14f3e2a7b179cd07c2d
Alex Sims <alex@softgrow.com>
2014-11-27 15:14:10 +1030

    add temperature bands

--- makecomb.jl
+++ makecomb.jl
@@ -42,24 +42,37 @@ for thiselem in d2[2:end]
  end
 outfil=open("comb.csv","w")
 # print a header
-println(outfil,"Fire,Incdate,dayofweek,hour,month,temperature,IsMonday,IsTuesday,IsWednesday,IsThursday,IsFriday,IsSaturday,IsSunday,IsMonday");
+println(outfil,"Fire,Incdate,dayofweek,hour,month,temperature,IsMonday,IsTuesday,IsWednesday,IsThursday,IsFriday,IsSaturday,IsSunday,tempband1,tempband2,tempband3,tempband4,tempband5a,tempband5b1,tempband5b2,tempband5b3,tempband5b4,tempband5b5,tempband5bz");
  # Iterate through each time loading a fire or not
 thisdate=DateTime(2009,5,1,0,0,0,0)
 zeroskipped=0
 linesprinted=0
 while thisdate < DateTime(2014,5,1,0,0,0,0)
   zeroskipped=zeroskipped+1
+  # 12 15 18 22
+  current_temp=get_adelaide_temp(thisdate)
+  tempband1=current_temp<12 ? 12-current_temp : 0
+  tempband2=current_temp>12 ? (current_temp<15 ? current_temp-12 : 15-12) : 0
+  tempband3=current_temp>15 ? (current_temp<18 ? current_temp-15 : 18-15) : 0
+  tempband4=current_temp>18 ? (current_temp<22 ? current_temp-18 : 22-18) : 0
+  tempband5a=current_temp>22 ? (current_temp<25 ? current_temp-22 : 25-22) : 0
+  tempband5b1=current_temp>25 ? (current_temp<28 ? current_temp-25 : 28-25) : 0
+  tempband5b2=current_temp>28 ? (current_temp<31 ? current_temp-28 : 31-28) : 0
+  tempband5b3=current_temp>31 ? (current_temp<34 ? current_temp-31 : 34-31) : 0
+  tempband5b4=current_temp>34 ? (current_temp<37 ? current_temp-34 : 37-34) : 0
+  tempband5b5=current_temp>37 ? (current_temp<40 ? current_temp-37 : 40-37) : 0
+  tempband5bz=current_temp>40 ? current_temp-40 : 0
   if (haskey(calllog,thisdate))
     if linesprinted < 1
       print(outfil,"1")
-      println(outfil,",\"",thisdate,"\",",dayofweek(thisdate),',',hour(thisdate),',',month(thisdate),',',get_adelaide_temp(thisdate),',',dayofweek(thisdate)==1?1:0,',',dayofweek(thisdate)==2?1:0,',',dayofweek(thisdate)==3?1:0,',',dayofweek(thisdate)==4?1:0,',',dayofweek(thisdate)==5?1:0,',',dayofweek(thisdate)==6?1:0,',',dayofweek(thisdate)==7?1:0)
+      println(outfil,",\"",thisdate,"\",",dayofweek(thisdate),',',hour(thisdate),',',month(thisdate),',',current_temp,',',dayofweek(thisdate)==1?1:0,',',dayofweek(thisdate)==2?1:0,',',dayofweek(thisdate)==3?1:0,',',dayofweek(thisdate)==4?1:0,',',dayofweek(thisdate)==5?1:0,',',dayofweek(thisdate)==6?1:0,',',dayofweek(thisdate)==7?1:0,',',tempband1,',',tempband2,',',tempband3,',',tempband4,',',tempband5a,',',tempband5b1,',',tempband5b2,',',tempband5b3,',',tempband5b4,',',tempband5b5,',',tempband5bz)
       linesprinted=linesprinted+1
     end
    end
-  if zeroskipped >=10
+  if zeroskipped >=1
     if linesprinted < 1
         print(outfil,"0")
-        println(outfil,",\"",thisdate,"\",",dayofweek(thisdate),',',hour(thisdate),',',month(thisdate),',',get_adelaide_temp(thisdate),',',dayofweek(thisdate)==1?1:0,',',dayofweek(thisdate)==2?1:0,',',dayofweek(thisdate)==3?1:0,',',dayofweek(thisdate)==4?1:0,',',dayofweek(thisdate)==5?1:0,',',dayofweek(thisdate)==6?1:0,',',dayofweek(thisdate)==7?1:0)
+      println(outfil,",\"",thisdate,"\",",dayofweek(thisdate),',',hour(thisdate),',',month(thisdate),',',current_temp,',',dayofweek(thisdate)==1?1:0,',',dayofweek(thisdate)==2?1:0,',',dayofweek(thisdate)==3?1:0,',',dayofweek(thisdate)==4?1:0,',',dayofweek(thisdate)==5?1:0,',',dayofweek(thisdate)==6?1:0,',',dayofweek(thisdate)==7?1:0,',',tempband1,',',tempband2,',',tempband3,',',tempband4,',',tempband5a,',',tempband5b1,',',tempband5b2,',',tempband5b3,',',tempband5b4,',',tempband5b5,',',tempband5bz)
       end
       zeroskipped=0
       linesprinted=0
