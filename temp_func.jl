2c32122564b515fef0fa7897d132ae101d776d36
Alex Sims <alex@softgrow.com>
2014-12-14 08:09:05 +0800

    use utc offset

--- temp_func.jl
+++ temp_func.jl
@@ -1,4 +1,43 @@
-function make_sure_data_loaded(current_time)
+function before_first_sunday(current_time::DateTime)
+  return day(current_time)<Dates.Sunday && day(current_time)>=dayofweek(current_time)
+end
+
+function get_utc_time(current_time::DateTime, airport_code::ASCIIString)
+  if airport_code=="ADL"
+    # NB doesn't work for before 2am
+    # First Sunday in October	First Sunday in April
+    if month(current_time)>Dates.April && month(current_time)<Dates.October
+      hour_offset=9
+    elseif month(current_time)<Dates.April || month(current_time)>Dates.October
+      hour_offset=10 # have daylight saving
+    elseif month(current_time)==Dates.April
+      # special case for April
+      if before_first_sunday(current_time)
+        hour_offset=10
+      else
+        hour_offset=9
+      end
+    elseif month(current_time)==Dates.October
+      # special case for October
+      if before_first_sunday(current_time)
+        hour_offset=9
+      else
+        hour_offset=10
+      end
+    else
+      error("logic error on month")
+    end
+    min_offset=30
+  elseif airport_code=="HND"
+    hour_offset=10
+    min_offset=0
+  else
+    error("Invalid airport code")
+  end
+  return current_time-Dates.Hour(hour_offset)-Dates.Minute(min_offset)
+end
+
+function make_sure_data_loaded(current_time::DateTime)
   if (!haskey(got_temp_year, year(current_time)))
     # we don't have this year loaded
     # Open the file, if we cant give up
@@ -6,10 +45,10 @@ function make_sure_data_loaded(current_time)
     wanted_site="946720"
     wanted_year=string(year(current_time))
     wanted_file="$wanted_site-99999-$wanted_year.gz"
-    if (!isfile(wanted_file))
+    if !isfile(wanted_file)
       # We need to fetch the file
       download("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/$wanted_year/$wanted_file", wanted_file)
-      if (!isfile(wanted_file))
+      if !isfile(wanted_file)
         error("Tried to download $source_file and that didn't work, giving up")
       end
     end
@@ -37,41 +76,38 @@ function make_sure_data_loaded(current_time)
   end
 end
 
-function get_adelaide_temp(current_time)
-  make_sure_data_loaded(current_time)
+function get_adelaide_temp(current_time::DateTime)
+  make_sure_data_loaded(get_utc_time(current_time,"ADL"))
   # find the time immediately before the current time and return the adelaide airport observation
-  # Dont forget to subtract 9.5 hours for UTC in the observations fro NASA
-  return adelaide_temp_obs[searchsortedlast(adelaide_temp_obs,current_time-Dates.Hour(9)-Dates.Minute(30))].mtemp
+  return adelaide_temp_obs[searchsortedlast(adelaide_temp_obs,get_utc_time(current_time,"ADL"))].mtemp
   # For the moment, just return the average max temperature for the month
   # average_mean_max=[28.1,28.1,25.5,22.2,18.6,15.9,14.9,15.9,18.2,21.0,23.9,25.7]
   #  return average_mean_max[month(current_time)]
 end
 
-function get_adelaide_dewp(current_time)
-  make_sure_data_loaded(current_time)
+function get_adelaide_dewp(current_time::DateTime)
+  make_sure_data_loaded(get_utc_time(current_time,"ADL"))
   # find the time immediately before the current time and return the adelaide airport observation
-  # Dont forget to subtract 9.5 hours for UTC in the observations fro NASA
-  return adelaide_dewp_obs[searchsortedlast(adelaide_dewp_obs,current_time-Dates.Hour(9)-Dates.Minute(30))].mtemp
+  return adelaide_dewp_obs[searchsortedlast(adelaide_dewp_obs,get_utc_time(current_time,"ADL"))].mtemp
 end
 
-function get_adelaide_wind_speed(current_time)
-  make_sure_data_loaded(current_time)
+function get_adelaide_wind_speed(current_time::DateTime)
+  make_sure_data_loaded(get_utc_time(current_time,"ADL"))
   # find the time immediately before the current time and return the adelaide airport observation
-  # Dont forget to subtract 9.5 hours for UTC in the observations fro NASA
-  return adelaide_wind_speed_obs[searchsortedlast(adelaide_wind_speed_obs,current_time-Dates.Hour(9)-Dates.Minute(30))].mtemp
+  return adelaide_wind_speed_obs[searchsortedlast(adelaide_wind_speed_obs,get_utc_time(current_time,"ADL"))].mtemp
 end
 
-function make_sure_tokyo_loaded(current_time)
-  if (!haskey(got_tokyo_year, year(current_time)))
+function make_sure_tokyo_loaded(current_time::DateTime)
+  if !haskey(got_tokyo_year, year(current_time))
     # we don't have this year loaded
     # Open the file, if we cant give up
     wanted_site="476710" # RJTT Haneda Tokyo International
     wanted_year=string(year(current_time))
     wanted_file="$wanted_site-99999-$wanted_year.gz"
-    if (!isfile(wanted_file))
+    if !isfile(wanted_file)
       # We need to fetch the file
       download("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/$wanted_year/$wanted_file", wanted_file)
-      if (!isfile(wanted_file))
+      if !isfile(wanted_file)
         error("Tried to download $source_file and that didn't work, giving up")
       end
     end
@@ -99,25 +135,22 @@ function make_sure_tokyo_loaded(current_time)
   end
 end
 
-function get_tokyo_temp(current_time)
-  make_sure_tokyo_loaded(current_time)
+function get_tokyo_temp(current_time::DateTime)
+  make_sure_tokyo_loaded(get_utc_time(current_time,"HND"))
   # find the time immediately before the current time and return the adelaide airport observation
-  # Dont forget to subtract 9 hours for UTC in the observations fro NASA
-  return tokyo_temp_obs[searchsortedlast(tokyo_temp_obs,current_time-Dates.Hour(9))].mtemp
+  return tokyo_temp_obs[searchsortedlast(tokyo_temp_obs,get_utc_time(current_time,"HND"))].mtemp
 end
 
-function get_tokyo_dewp(current_time)
-  make_sure_tokyo_loaded(current_time)
+function get_tokyo_dewp(current_time::DateTime)
+  make_sure_tokyo_loaded(get_utc_time(current_time,"HND"))
   # find the time immediately before the current time and return the adelaide airport observation
-  # Dont forget to subtract 9 hours for UTC in the observations fro NASA
-  return tokyo_dewp_obs[searchsortedlast(tokyo_dewp_obs,current_time-Dates.Hour(9))].mtemp
+  return tokyo_dewp_obs[searchsortedlast(tokyo_dewp_obs,get_utc_time(current_time,"HND"))].mtemp
 end
 
-function get_tokyo_wind_speed(current_time)
-  make_sure_tokyo_loaded(current_time)
+function get_tokyo_wind_speed(current_time::DateTime)
+  make_sure_tokyo_loaded(get_utc_time(current_time,"HND"))
   # find the time immediately before the current time and return the adelaide airport observation
-  # Dont forget to subtract 9.5 hours for UTC in the observations fro NASA
-  return tokyo_wind_speed_obs[searchsortedlast(tokyo_wind_speed_obs,current_time-Dates.Hour(9))].mtemp
+  return tokyo_wind_speed_obs[searchsortedlast(tokyo_wind_speed_obs,get_utc_time(current_time,"HND"))].mtemp
 end
 
 got_temp_year=Dict{Int,Bool}()
